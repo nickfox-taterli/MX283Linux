2dfb5bd9ebc4080ab7b2d8766730f096d56e9d4c
TaterLi <admin@lijingquan.net>
2017-09-29 13:19:05 +0800

    修正显示扫描频率和QT5触摸特性,增加rootfs.

--- u-boot/include/configs/mx28evk.h
+++ u-boot/include/configs/mx28evk.h
@@ -128,7 +128,7 @@
 
 /* Extra Environment */
 #define CONFIG_EXTRA_ENV_SETTINGS \
-	"videomode=video=ctfb:x:480,y:272,depth:16,mode:0,pclk:12000, le:8,ri:4,up:2,lo:4,hs:10,vs:10,sync:0,vmode:0\0" \
+	"videomode=video=ctfb:x:480,y:272,depth:16,mode:0,pclk:80000, le:8,ri:4,up:2,lo:4,hs:10,vs:10,sync:0,vmode:0\0" \
 	"ubifs_file=filesystem.ubifs\0" \
 	"nandargs=setenv bootargs console=${console_mainline},${baudrate} " \
 		"rootfstype=ubifs ubi.mtd=6 root=ubi0_0 ${mtdparts} rw\0" \
